5eabecce44e067055e51dee1c5d6e92b139ad9f7
abe menashy <abestbet1@gmail.com>
2021-07-13 20:26:05 -0700

    add js file

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+const body = document.querySelector('body');
+const mobileNav = document.querySelector('.header__nav-container');
+const hamburgerToggle = document.getElementById('mobile-nav');
+
+hamburgerToggle.addEventListener('click', () => {
+  console.log('click');
+  if (hamburgerToggle.className === 'fas fa-bars mobile-nav') {
+    hamburgerToggle.className = 'fas fa-times mobile-nav elevate-element';
+    mobileNav.classList.add('show-mobile-nav');
+    body.classList.add('no-scroll');
+  } else {
+    hamburgerToggle.className = 'fas fa-bars mobile-nav';
+    mobileNav.classList.remove('show-mobile-nav');
+    body.classList.remove('no-scroll');
+  }
+});
