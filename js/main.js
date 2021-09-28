4093579c52d527a59ebe34a9aaf0493e6f2f9f7c
abe menashy <abestbet1@gmail.com>
2021-09-27 18:21:41 -0700

    fix mobile view

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
-
 const body = document.querySelector('body');
 const mobileNav = document.querySelector('.header__link-container');
 const hamburgerToggle = document.getElementById('mobile-nav');
-
-const productDropdown = document.getElementById('product-header');
-const productChevron = document.getElementById('product');
-const productMenu = document.querySelector('.nav-products');
-
-const companyDropdown = document.getElementById('company-header');
-const companyChevron = document.getElementById('company');
-const companyMenu = document.querySelector('.nav-company');
-
-const connectDropdown = document.getElementById('connect-header');
-const connectChevron = document.getElementById('connect');
-const connectMenu = document.querySelector('.nav-connect');
 
 hamburgerToggle.addEventListener('click', () => {
   if (hamburgerToggle.className === 'fas fa-bars mobile-nav') {
@@ -26,21 +13,3 @@ hamburgerToggle.addEventListener('click', () => {
     body.classList.remove('no-scroll');
   }
 });
-
-// productDropdown.addEventListener('click', () => {
-//   productChevron.classList.toggle('fa-chevron-down')
-//   productChevron.classList.toggle('fa-chevron-up')
-//   productMenu.classList.toggle("hide");
-// });
-
-// connectDropdown.addEventListener('click', () => {
-//   connectChevron.classList.toggle('fa-chevron-down')
-//   connectChevron.classList.toggle('fa-chevron-up')
-//   connectMenu.classList.toggle("hide");
-// });
-
-// companyDropdown.addEventListener('click', () => {
-//   companyChevron.classList.toggle('fa-chevron-down')
-//   companyChevron.classList.toggle('fa-chevron-up')
-//   companyMenu.classList.toggle("hide");
-// });
